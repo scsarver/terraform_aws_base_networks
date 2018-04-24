ef82de88f6f51da77ad9b76d8b5965dc740bcfc3
Stephan Sarver <stephan.sarver@gmail.com>
2018-04-24 01:02:22 -0500

    Added subnet id outputs.

--- vpc_2pub_az_2priv_az/output.tf
+++ vpc_2pub_az_2priv_az/output.tf
@@ -5,3 +5,19 @@ output "primary_vpc_id" {
 output "primary_vpc_cidr_block" {
   value = "${aws_vpc.primary_zone.cidr_block}"
 }
+
+output "public_primary_subnet_id" {
+  value = "${aws_subnet.primary_zone_primary_public_subnet.id}"
+}
+
+output "public_secondary_subnet_id" {
+  value = "${aws_subnet.primary_zone_secondary_public_subnet.id}"
+}
+
+output "private_primary_subnet_id" {
+  value = "${aws_subnet.primary_zone_primary_private_subnet.id}"
+}
+
+output "private_secondary_subnet_id" {
+  value = "${aws_subnet.primary_zone_secondary_private_subnet.id}"
+}
