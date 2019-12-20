575ce265e2ef0dc8cc171ec7359c10697b0a6be1
Stephan Sarver <stephan.sarver@gmail.com>
2019-12-20 16:20:27 -0600

    Updated s3.tf to trigger terraform cloud run.

--- terraform_cloud/organization_ous_scp_test/s3.tf
+++ terraform_cloud/organization_ous_scp_test/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "scsresearch-scp-test" {
-  bucket = "scsresearch-scp-test"
+  bucket = "scsresearch-scp-bucket-test"
   acl    = "private"
 
   # server_side_encryption_configuration {
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "scsresearch-scp-test" {
 }
 
 resource "aws_s3_bucket_object" "scsresearch-scp-test" {
-  key                    = "scsresearch-scp-test"
+  key                    = "scsresearch-scp-bucket-test"
   bucket                 = "${aws_s3_bucket.scsresearch-scp-test.id}"
   content                 = "This super secret text shall only be uploaded to s3 as an object if the serverside encryption is set otherwise SCP should reject it."
   server_side_encryption = "AES256"
