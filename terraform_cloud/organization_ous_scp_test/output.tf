30f2e213d0444b3b7c7bfad41dace8e5cdeb4a58
Stephan Sarver <stephan.sarver@gmail.com>
2019-12-20 15:55:52 -0600

    Added s3 object key output.

--- terraform_cloud/organization_ous_scp_test/output.tf
+++ terraform_cloud/organization_ous_scp_test/output.tf
@@ -7,3 +7,8 @@ output "scsresearch-scp-test-bucket-name" {
   description = "The name of the scp testing bucket."
   value = "${aws_s3_bucket.scsresearch-scp-test.name}"
 }
+
+output "scsresearch-scp-test-s3-object-key" {
+  description = "The key of the object to put in the scp testing bucket."
+  value = "${aws_s3_bucket_object.scsresearch-scp-test.key}"
+}
